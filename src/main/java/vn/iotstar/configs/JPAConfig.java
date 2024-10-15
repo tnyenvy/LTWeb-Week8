51a7622b153fd4f947ac5e26d674c1c32c0188d9
tnyenvy <tnyenvy.2003@gmail.com>
2024-10-15 20:02:02 +0700

    Week 8

---
+++ src/main/java/vn/iotstar/configs/JPAConfig.java
@@ -0,0 +1,23 @@
+package vn.iotstar.configs;
+
+import jakarta.persistence.EntityManager;
+
+import jakarta.persistence.EntityManagerFactory;
+
+import jakarta.persistence.Persistence;
+
+import jakarta.persistence.PersistenceContext;
+
+@PersistenceContext
+
+public class JPAConfig {
+
+	public static EntityManager getEntityManager() {
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("GG");
+
+		return factory.createEntityManager();
+
+	}
+
+}
